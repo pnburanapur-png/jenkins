8cfaf9a8d3858d063f0cfd793fdf5d7785aefd5b
pnburanapur-png <pnburanapur@gmail.com>
2026-03-27 11:07:15 +0530

    Update hello_wrold.java

--- hello_wrold.java
+++ hello_wrold.java
@@ -1,6 +1,6 @@
 class hello_wrold
     {
-public static void main(String []ags){
-    system.out.println("Hello wrold");
+public static void main(String []args){
+    System.out.println("Hello wrold");
 }
     }
